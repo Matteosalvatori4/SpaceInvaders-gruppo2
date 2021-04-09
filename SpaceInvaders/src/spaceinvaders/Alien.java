7c26606129df2baaf9c771faa0e1a1560b04a40e
Matteosalvatori4 <matteo.salvatori4@gmail.com>
2021-04-09 17:14:38 +0200

    Implementate classi alien e rocket

--- SpaceInvaders/src/spaceinvaders/Alien.java
+++ SpaceInvaders/src/spaceinvaders/Alien.java
@@ -5,10 +5,38 @@
  */
 package spaceinvaders;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author francesco
  */
 public class Alien extends ElementiDiGioco {
-    
+
+    public Alien(JButton button, double y) {
+        setButton(button);
+        setY(y);
+        setTargetX(NewJFrameSpaceInvaders.ALIEN_XMAX);
+        setTargetY(y);
+        setX(NewJFrameSpaceInvaders.ALIEN_XMIN);
+        setStepX(1);
+        setStepY(0);
+    }
+
+    @Override
+    public void move() {
+        // si muove a destra
+        if (getX() == NewJFrameSpaceInvaders.ALIEN_XMAX) {
+
+            setTargetX(NewJFrameSpaceInvaders.ALIEN_XMIN);
+        } //si muove a sinistra
+        else {
+            setTargetX(NewJFrameSpaceInvaders.ALIEN_XMAX);
+        }
+        //esegue il movimento
+        setStepX(-getStepX());
+    }
 }
+
+
--- SpaceInvaders/src/spaceinvaders/Rocket.java
+++ SpaceInvaders/src/spaceinvaders/Rocket.java
@@ -5,10 +5,33 @@
  */
 package spaceinvaders;
 
+import javax.swing.JButton;
+
 /**
  *
  * @author francesco
  */
 public class Rocket extends ElementiDiGioco {
+    
+    public Rocket(JButton button, double x) {
+        setButton(button);
+        getButton().setVisible(true);
+        setX(x);
+        setY(NewJFrameSpaceInvaders.ROCKET_YMIN);
+        setStepX(0);
+        setStepY(-2);
+    }
+    
+    @Override
+    public void move() {
+        //controllo margine superiore
+        if (getY() == NewJFrameSpaceInvaders.ROCKET_YMAX) {
+            getButton().setVisible(false);
+            setStepX(0);
+            setStepY(0);
+        } else {
+            setTargetX(NewJFrameSpaceInvaders.ROCKET_YMIN);
+        }
+    }
     
 }
